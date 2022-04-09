3b4fb42037a77653293f3a0e1416fbb128faf614
oustella <oustella@gmail.com>
2022-04-09 00:54:13 -0400

    segmented three views

---
+++ Civica/RepDetailTweetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepDetailTweetTableViewCell.swift
+//  Civica
+//
+//  Created by OSL on 4/6/22.
+//
+
+import UIKit
+
+class RepDetailTweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
