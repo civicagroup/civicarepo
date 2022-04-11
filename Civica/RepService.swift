a82702e26e034e1fb7333216813cc563a210b913
Antonella <avog001@fiu.edu>
2022-04-11 10:27:29 -0400

    first commit to branch

--- Civica/RepService.swift
+++ Civica/RepService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+
 class RepService {
     static let shared = RepService()
     
@@ -14,7 +15,9 @@ class RepService {
        
         let CIVIC_API_KEY = "AIzaSyCC2MxPRX7kj6Mg6e7eaYaHGMKZWkNb8Jg"
         let address = "1600 Pennsylvania Avenue Northwest, Washington, DC, 20500"
-        let url = URL(string: "https://www.googleapis.com/civicinfo/v2/representatives?key=\(CIVIC_API_KEY)&address=\(address)")!
+        let addressFixed:String = address.replacingOccurrences(of: " ", with: "%20")
+        let url = URL(string: "https://www.googleapis.com/civicinfo/v2/representatives?key=\(CIVIC_API_KEY)&address=\(addressFixed)")!
+        
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -34,12 +37,12 @@ class RepService {
                  let repsRawData = dataDictionary["officials"] as! [[String: Any]]
                  var representatives = [Representative]()
                  for rawData in repsRawData {
-                     let representative = Representative(name: rawData["name"] as! String
+                     let representative = Representative(name: rawData["name"] as! String,
 //                                                         address: rawData["address"]["line1"] as! String,
-//                                                         party: rawData["party"] as! String,
-//                                                         phone: rawData["phones"] as! String,
-//                                                         website: rawData["urls"] as! String,
-//                                                         email: rawData["emails"] as! String
+                                                         party: rawData["party"] as! String,
+                                                         phones: rawData["phones"] as! [String]
+                                                         //website: rawData["urls"] as! String,
+                                                         //emails: rawData["emails"] as! [String]
 //                                                         imageURL: rawData[""]
                      )
                      representatives.append(representative)
--- Civica/Representative.swift
+++ Civica/Representative.swift
@@ -7,19 +7,92 @@
 
 import Foundation
 
-struct Representative {
+struct Representative: Codable {
     let name: String
-//    let party: String?
-//    let phone: String?
-//    let website: String?
-//    let email: String?
+    let party: String
+//    let address: address
+    let phones: [String]
+//    let website: String
+    //let emails: [String]
     
-//    struct address: Codable {
-//        let line1: String
-//        let line2: String
-//        let city: String
-//        let state: String
-//        let zip: String
-//    }
+    struct address: Codable {
+        let line1: String
+        let line2: String
+        let city: String
+        let state: String
+        let zip: String
+    }
+    
+ 
     //let imageURL: String
 }
+
+
+/*{
+ "divisions": {
+   (key): {
+     "name": string,
+     "alsoKnownAs": [
+       string
+     ],
+     "officeIndices": [
+       unsigned integer
+     ]
+   }
+ },
+ "offices": [
+   {
+     "name": string,
+     "divisionId": string,
+     "levels": [
+       string
+     ],
+     "roles": [
+       string
+     ],
+     "sources": [
+       {
+         "name": string,
+         "official": boolean
+       }
+     ],
+     "officialIndices": [
+       unsigned integer
+     ]
+   }
+ ],
+ "officials": [
+   {
+     "name": string,
+     "address": [
+       {
+         "locationName": string,
+         "line1": string,
+         "line2": string,
+         "line3": string,
+         "city": string,
+         "state": string,
+         "zip": string
+       }
+     ],
+     "party": string,
+     "phones": [
+       string
+     ],
+     "urls": [
+       string
+     ],
+     "photoUrl": string,
+     "emails": [
+       string
+     ],
+     "channels": [
+       {
+         "type": string,
+         "id": string
+       }
+     ]
+   }
+ ]
+}
+ */
