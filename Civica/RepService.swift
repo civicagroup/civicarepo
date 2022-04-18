7549d2bc077bd6a68169991f52c62e25d7186ef8
Antonella <avog001@fiu.edu>
2022-04-18 18:56:36 -0400

    working further on retrieving rep data from API

--- Civica/RepService.swift
+++ Civica/RepService.swift
@@ -2,59 +2,59 @@
 //  RepService.swift
 //  Civica
 //
-//  Created by Antonella on 4/11/22.
+////  Created by Antonella on 4/11/22.
+////
 //
+//import Foundation
 
-import Foundation
 
-
-class RepService {
-    
-    
-    static let shared = RepService()
-    
-//    func fetchReps(address: String, completion: @escaping((Representative?) -> Void)) {
-    func fetchReps(completion: @escaping((Representative?) -> Void)) {
-       
-        let CIVIC_API_KEY = "AIzaSyCC2MxPRX7kj6Mg6e7eaYaHGMKZWkNb8Jg"
-        let address = "1600 Pennsylvania Avenue Northwest, Washington, DC, 20500"
-        let addressFixed:String = address.replacingOccurrences(of: " ", with: "%20")
-        let url = URL(string: "https://www.googleapis.com/civicinfo/v2/representatives?key=\(CIVIC_API_KEY)&address=\(addressFixed)")!
-        
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                let decoder = JSONDecoder()
-                let repsList = try? decoder.decode(Representative.self, from: data)
-                completion(repsList)
-            }
-
-//             if let error = error {
-//                    print(error.localizedDescription)
-//             } else if let data = data {
-//                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-//                 let repsRawData = dataDictionary["officials"] as! [[String: Any]]
-//                 var representatives = [Representative]()
-//                 for rawData in repsRawData {
-//                     let representative = Representative(name: rawData["name"] as! String,
-////                                                         address: rawData["address"]["line1"] as! String,
-//                                                         party: rawData["party"] as! String,
-//                                                         phones: rawData["phones"] as! [String]
-//                                                         //website: rawData["urls"] as! String,
-//                                                         //emails: rawData["emails"] as! [String]
-////                                                         imageURL: rawData[""]
-//                     )
-//                     representatives.append(representative)
-//                 }
-//                 completion(representatives)
-//             }
-        }
-        task.resume()
-        
-    }
-
-}
-
+//class RepService {
+//
+//
+//    static let shared = RepService()
+//
+////    func fetchReps(address: String, completion: @escaping((Representative?) -> Void)) {
+//    func fetchReps(completion: @escaping((RepResponse?) -> Void)) {
+//
+//        let CIVIC_API_KEY = "AIzaSyCC2MxPRX7kj6Mg6e7eaYaHGMKZWkNb8Jg"
+//        let address = "1600 Pennsylvania Avenue Northwest, Washington, DC, 20500"
+//        let addressFixed:String = address.replacingOccurrences(of: " ", with: "%20")
+//        let url = URL(string: "https://www.googleapis.com/civicinfo/v2/representatives?key=\(CIVIC_API_KEY)&address=\(addressFixed)")!
+//
+//        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+//        let task = session.dataTask(with: request) { (data, response, error) in
+//            if let error = error {
+//                print(error.localizedDescription)
+//            } else if let data = data {
+//                let decoder = JSONDecoder()
+//                let repsList = try? decoder.decode(RepResponse.self, from: data)
+//                completion(repsList)
+//            }
+//
+////             if let error = error {
+////                    print(error.localizedDescription)
+////             } else if let data = data {
+////                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+////                 let repsRawData = dataDictionary["officials"] as! [[String: Any]]
+////                 var representatives = [Representative]()
+////                 for rawData in repsRawData {
+////                     let representative = Representative(name: rawData["name"] as! String,
+//////                                                         address: rawData["address"]["line1"] as! String,
+////                                                         party: rawData["party"] as! String,
+////                                                         phones: rawData["phones"] as! [String]
+////                                                         //website: rawData["urls"] as! String,
+////                                                         //emails: rawData["emails"] as! [String]
+//////                                                         imageURL: rawData[""]
+////                     )
+////                     representatives.append(representative)
+////                 }
+////                 completion(representatives)
+////             }
+//        }
+//        task.resume()
+//
+//    }
+//
+//}
+//
