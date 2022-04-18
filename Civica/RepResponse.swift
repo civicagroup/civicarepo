238a7af2d676d605c4b6bbf00a7f57327b203460
Antonella <avog001@fiu.edu>
2022-04-17 22:07:52 -0400

    created structs for reps data

--- Civica/RepResponse.swift
+++ Civica/RepResponse.swift
@@ -7,10 +7,49 @@
 
 import Foundation
 
-struct division {
+//struct divisions {
+//
+//}
+
+//struct normalizedInput{
+//
+//}
+
+
+struct offices: Decodable {
+    let name: String
+    let divisionId: String
+    let levels: [String]
+    let roles: [String]
+    let officialIndices: [Int]
     
 }
 
-struct RepResponse {
+struct officials: Decodable {
+    let name: String
+    let address: [address]
+    let party: String
+    let phones: [String]
+    let urls: [String]
+    let channels: [channels]
+    let emails: [String]
     
+}
+
+struct channels: Decodable {
+    let type: String
+    let id: String
+}
+
+struct address: Decodable {
+    let line1: String
+    let city: String
+    let state: String
+    let zip: String
+}
+
+struct RepResponse: Decodable {
+    let kind: String
+    let offices: [offices]
+    let officials: [officials]
 }
