bcde0c10ff387c2343656f5671513629de98d307
oustella <oustella@gmail.com>
2022-04-21 00:17:48 -0400

    Create Event List

---
+++ Civica/EventDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventDetailTableViewCell.swift
+//  Civica
+//
+//  Created by OSL on 4/20/22.
+//
+
+import UIKit
+
+class EventDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Civica/MyUtil.swift
@@ -0,0 +1,8 @@
+//
+//  MyUtil.swift
+//  Civica
+//
+//  Created by OSL on 4/20/22.
+//
+
+import Foundation
